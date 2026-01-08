85562b7108abba96e1308ba5a68b42f7f39f68b3
Edrian Hernandez <117151322+EdrianHernandez@users.noreply.github.com>
2026-01-08 15:03:50 +0800

    Update and rename types.ts to types.js

---
+++ types.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {Object} Car
+ * @property {string} id
+ * @property {string} name
+ * @property {string} category
+ * @property {number} pricePerDay
+ * @property {string} image
+ * @property {string[]} specs
+ */
+
+/**
+ * @typedef {Object} BookingDetails
+ * @property {string} pickupLocation
+ * @property {string} returnLocation
+ * @property {string} pickupDate
+ * @property {string} returnDate
+ */
